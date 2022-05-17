0eca2220eb58e18ffa52fab8a5ae83b631450ad3
CelineBarbe <laloutrecosmique@hotmail.fr>
2022-05-17 10:53:17 +0200

    console log names

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ type Spots struct {
 }
 
 type Spot struct {
-	Name        string `json:"spot"`
+	Name        string `json:"name"`
 	ID          int    `json:"id"`
 	Coordinates struct {
 		Longitude float64 `json:"longitude"`
@@ -31,28 +31,35 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the surf!")
 }
 
-func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homeLink)
-	log.Fatal(http.ListenAndServe(":8080", router))
-
+func printJson(w http.ResponseWriter, r *http.Request) {
 	// Open our jsonFile
 	jsonFile, err := os.Open("spots.json")
 	// if we os.Open returns an error then handle it
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("Successfully Opened users.json")
+	fmt.Println("Successfully Opened spots.json")
+	
 	// defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
+	
+	// read our opened xmlFile as a byte array.
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	
+	// we initialize our Users array
+	var spots Spots
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'users' which we defined above
+	json.Unmarshal(byteValue, &spots)
 
-    // read our opened xmlFile as a byte array.
-    byteValue, _ := ioutil.ReadAll(jsonFile)
+	for i := 0; i < len(spots.Spots); i++ {
+		fmt.Println("Spot Name: " + spots.Spots[i].Name)
+	}
+}
 
-    // we initialize our Users array
-    var spots Spots
-
-    // we unmarshal our byteArray which contains our
-    // jsonFile's content into 'users' which we defined above
-    json.Unmarshal(byteValue, &spots)
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/spots", printJson)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
